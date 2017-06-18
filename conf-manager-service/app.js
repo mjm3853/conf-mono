196f8639508159549e287b2c2ad7007770e6038f
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-06-18 17:04:47 -0400

    some express structure

--- conf-manager-service/app.js
+++ conf-manager-service/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var conf_index = require('./routes/conf_index');
+var conf_get = require('./routes/conf_get');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/api/conf', conf_index);
+app.use('/api/conf/get', conf_get);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
