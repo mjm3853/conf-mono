4886f9aee0fc4579af521317632572a4347bd601
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-06-19 11:49:43 -0400

    refactored db setup to promise

--- conf-manager-service/lib/rethinkdb.js
+++ conf-manager-service/lib/rethinkdb.js
@@ -1,6 +1,6 @@
 var r = require('rethinkdb'),
-    util = require('util'),
-    assert = require('assert');
+  util = require('util'),
+  assert = require('assert');
 
 // #### Connection details
 
@@ -8,7 +8,7 @@ var r = require('rethinkdb'),
 var dbConfig = {
   host: process.env.RDB_HOST || 'localhost',
   port: parseInt(process.env.RDB_PORT) || 28015,
-  db  : process.env.RDB_DB || 'conf',
+  db: process.env.RDB_DB || 'conf',
   tables: {
     'confs': 'id'
   }
@@ -20,20 +20,19 @@ var dbConfig = {
  * - create the `RDB_DB` database (defaults to `conf`)
  * - create table `confs` in this database
  */
-module.exports.setup = function() {
-  r.connect({host: dbConfig.host, port: dbConfig.port }, function (err, connection) {
-    assert.ok(err === null, err);
-    r.dbCreate(dbConfig.db).run(connection, function(err, result) {
-      if(err) {
+module.exports.setup = function () {
+  r.connect({ host: dbConfig.host, port: dbConfig.port }).then(function (connection) {
+    r.dbCreate(dbConfig.db).run(connection, function (err, result) {
+      if (err) {
         console.log("[DEBUG] RethinkDB database '%s' already exists (%s:%s)\n%s", dbConfig.db, err.name, err.msg, err.message);
       }
       else {
         console.log("[INFO ] RethinkDB database '%s' created", dbConfig.db);
       }
-      for(var tbl in dbConfig.tables) {
+      for (var tbl in dbConfig.tables) {
         (function (tableName) {
-          r.db(dbConfig.db).tableCreate(tableName, {primaryKey: dbConfig.tables[tbl]}).run(connection, function(err, result) {
-            if(err) {
+          r.db(dbConfig.db).tableCreate(tableName, { primaryKey: dbConfig.tables[tbl] }).run(connection, function (err, result) {
+            if (err) {
               console.log("[DEBUG] RethinkDB table '%s' already exists (%s:%s)\n%s", tableName, err.name, err.msg, err.message);
             }
             else {
@@ -43,8 +42,10 @@ module.exports.setup = function() {
         })(tbl);
       }
     });
-  });
-};
+  }).catch(function (err) {
+    return err;
+  })
+}
 
 /**
  * Gets a list of conferences equal to or less than the specified max_results
@@ -59,15 +60,15 @@ module.exports.setup = function() {
  */
 module.exports.getConferences = function (max_results, callback) {
   onConnect(function (err, connection) {
-    r.db(dbConfig['db']).table('confs').limit(max_results).run(connection, function(err, cursor) {
-      if(err) {
+    r.db(dbConfig['db']).table('confs').limit(max_results).run(connection, function (err, cursor) {
+      if (err) {
         console.log("[ERROR][%s][getConferences] %s:%s\n%s", connection['_id'], err.name, err.msg, err.message);
         callback(null, []);
         connection.close();
       }
       else {
-        cursor.toArray(function(err, results) {
-          if(err) {
+        cursor.toArray(function (err, results) {
+          if (err) {
             console.log("[ERROR][%s][getConferences][toArray] %s:%s\n%s", connection['_id'], err.name, err.msg, err.message);
             callback(null, []);
           }
@@ -87,11 +88,11 @@ module.exports.getConferences = function (max_results, callback) {
  * A wrapper function for the RethinkDB API `r.connect`
  * to keep the configuration details in a single function
  * and fail fast in case of a connection error.
- */ 
+ */
 function onConnect(callback) {
-  r.connect({host: dbConfig.host, port: dbConfig.port }, function(err, connection) {
+  r.connect({ host: dbConfig.host, port: dbConfig.port }, function (err, connection) {
     assert.ok(err === null, err);
-    connection['_id'] = Math.floor(Math.random()*10001);
+    connection['_id'] = Math.floor(Math.random() * 10001);
     callback(err, connection);
   });
 }
