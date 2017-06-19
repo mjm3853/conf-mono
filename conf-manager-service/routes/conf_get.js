d8e61074bef08643050df9a44eb9ad59d0d05d75
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-06-19 14:46:02 -0400

    winston custom logger added

--- conf-manager-service/routes/conf_get.js
+++ conf-manager-service/routes/conf_get.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/rethinkdb');
+var logger = require('../lib/logger');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   db.getConferences(5, function (err, results) {
     if (err) {
+      logger.error(err);
       res.status(500).send(err);
     } else {
       res.send(results);
