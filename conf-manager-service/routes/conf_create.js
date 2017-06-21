f1b2e0aba5afb9c87c805f190175608af735f689
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-06-20 22:11:40 -0400

    backgroud color update

--- conf-manager-service/routes/conf_create.js
+++ conf-manager-service/routes/conf_create.js
@@ -6,15 +6,15 @@ var logger = require('../lib/logger');
 /* Creat Conference. */
 router.post('/conference', function (req, res, next) {
   var conference_details = {
-    name: "Mock Auto Created",
+    name: "Another Mock Auto Created Again",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    location: "Boston, MA",
+    location: "Manchester, NH",
     tags: [
-      "angular",
-      "babel"
+      "react",
+      "redux"
     ],
     url: "https://en.wikipedia.org/wiki/Conference",
-    date: "June 25, 2017"
+    date: "September 25, 2017"
   }
   db.createConference(conference_details, function (err, outcome) {
     if (err) {
