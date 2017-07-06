6311fb7b81e2df6cdb68c15d83a281d5ea50a19a
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-07-05 22:54:45 -0400

    some tachyon

--- conf-apollo/src/App.js
+++ conf-apollo/src/App.js
@@ -5,8 +5,7 @@ import {
   graphql,
   ApolloProvider,
   createNetworkInterface
-} from 'react-apollo'
-import './App.css';
+} from 'react-apollo';
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface('https://api.graph.cool/simple/v1/conf-ql'),
@@ -18,6 +17,21 @@ const conferencesListQuery = gql`
       id
       name
       description
+      start
+      end
+      location {
+        id
+        name
+        city
+        state
+        country
+        postalCode
+        address
+      }
+      tags {
+        id
+        name
+      }
     }
   }
 `;
@@ -30,13 +44,23 @@ const ConferencesList = ({ data: { loading, error, allConferences } }) => {
     return <p>{error.message}</p>;
   }
 
-  return <ul className="Item-list">
+  return <ul className="pt5">
     {allConferences
-      .map(conf => 
-        <li key={conf.id}>
-          <p>{conf.name}</p>
-          <p>{conf.description}</p>
-        </li>)}
+      .map(conf =>
+        <article key={conf.id} className="center mw5 mw6-ns hidden ba mv4">
+          <h1 className="f4 bg-near-black white mv0 pv2 ph3">{conf.name}</h1>
+          <div className="pa3 bt">
+            <p className="f6 f5-ns lh-copy measure mv0">{conf.description}</p>
+            <p className="f6 f5-ns lh-copy measure mv0">From {conf.start} to {conf.end}</p>
+            <p className="f6 f5-ns lh-copy measure mv0">{conf.location.city}, {conf.location.state}</p>
+            <ul>
+            {conf.tags
+              .map(tag =>
+                <li className="f6 link dim br-pill ba ph3 pv2 mb2 dib black" key={tag.id}>{tag.name}</li>
+              )}
+          </ul>
+          </div>
+        </article>)}
   </ul>;
 };
 
@@ -47,9 +71,11 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div className="App">
-          <div className="App-header">
-            <h2>Welcome to conf-apollo</h2>
-          </div>
+          <header className="bg-black-90 fixed w-100 ph3 pv3 top-0 pv4-ns ph4-m ph5-l">
+            <nav className="f6 fw6 ttu tracked">
+              <a className="link dim white dib mr3" href="#" title="Home">Home</a>
+            </nav>
+          </header>
           <ConferencesListWithData />
         </div>
       </ApolloProvider>
