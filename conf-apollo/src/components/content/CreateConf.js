679a63c69471767a9ccd205f8026954171ac77a9
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-07-11 23:33:41 -0400

    submit from form

--- conf-apollo/src/components/content/CreateConf.js
+++ conf-apollo/src/components/content/CreateConf.js
@@ -41,47 +41,67 @@ const createConferenceMutation = gql`
 }
 `;
 
-const CreateConference = ({ mutate }) => {
-  const handleKeyUp = (evt) => {
-    if (evt.keyCode === 13) {
-      evt.persist();
-      mutate({
-        variables: { 
-          name: evt.target.value,
-          description: "Submitted from input",
-          start: "2017-07-12T04:00:00.000Z",
-          end: "2017-07-12T04:00:00.000Z",
-          locationName: "InputLand",
-          locationCity: "abc",
-          locationState: "xyz",
-          tags: [
-            {
-              name: "first"
-            },
-            {
-              name: "second"
-            }
-          ]
-         }
-      })
-        .then(res => {
-          evt.target.value = '';
-        });
+class CreateConference extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      description: "Submitted from Form",
+      start: "2017-07-12T04:00:00.000Z",
+      end: "2017-07-12T04:00:00.000Z",
+      locationName: "InputLand",
+      locationCity: "abc",
+      locationState: "xyz",
+      tags: [
+        {
+          name: "Form"
+        },
+        {
+          name: "Apollo"
+        }
+      ]
     }
-  };
-  return (
-    <ul className="pt5">
-      <article className="center mw5 mw6-ns hidden ba mv4">
-        <h1 className="f4 bg-near-black white mv0 pv2 ph3">Create Conference</h1>
-        <input
-          type="text"
-          placeholder="New channel"
-          onKeyUp={handleKeyUp}
-        />
-      </article>
-    </ul>
-  );
-};
+  }
+  handleNameChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.mutate({
+      variables: {
+        name: this.state.name,
+        description: this.state.description,
+        start: this.state.start,
+        end: this.state.end,
+        locationName: this.state.locationName,
+        locationCity: this.state.locationCity,
+        locationState: this.state.locationState,
+        tags: this.state.tags
+      }
+    })
+      .then(res => {
+        this.setState({
+          name: ""
+        })
+      });
+  }
+  render() {
+    return (
+      <form className="pt5" onSubmit={this.handleSubmit.bind(this)}>
+        <div className="center mw5 mw6-ns hidden ba mv4">
+          <h1 className="f4 bg-near-black white mv0 pv2 ph3">Create Conference</h1>
+          <label>
+            Name:
+          <input type="text" onChange={this.handleNameChange.bind(this)} value={this.state.name} />
+          </label>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    );
+  }
+}
 
 const CreateConferenceWithData = graphql(createConferenceMutation)(CreateConference);
 
